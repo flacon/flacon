cb2114affa14a6000774c97ed915ac55a814e809
sokoloff (Alexander) <sokoloff.a@gmail.com>
2015-12-03 19:21:10 +0300

    Translations

--- inputaudiofile.cpp
+++ inputaudiofile.cpp
@@ -110,8 +110,8 @@ bool InputAudioFile::load()
 
     if (!QFileInfo(mFileName).exists())
     {
-        qWarning() << QString("Audio file <b>\"%1\"</b> does not exists").arg(mFileName);
-        mErrorString = QObject::tr("Audio file <b>\"%1\"</b> does not exists").arg(mFileName);
+        qWarning() << QString("The Audio file <b>\"%1\"</b> does not exist").arg(mFileName);
+        mErrorString = QObject::tr("The Audio file <b>\"%1\"</b> does not exist").arg(mFileName);
         return false;
     }
 
