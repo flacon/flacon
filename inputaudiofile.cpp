3d4bcfc52f5a1fa1378a41f1151a5da1781ba2ef
sokoloff (Alexander) <sokoloff.a@gmail.com>
2016-01-30 21:03:06 +0300

    Small fixes for support -Werror=format-security fixses

--- inputaudiofile.cpp
+++ inputaudiofile.cpp
@@ -136,7 +136,7 @@ bool InputAudioFile::load()
         qWarning("------------------------------------");
         qWarning() << "Test audio command:" << (shntool + " " + args.join(" "));
         qWarning() << "shntool info waitForFinished faild";
-        qWarning(proc.readAllStandardError());
+        qWarning() << proc.readAllStandardError();
         qWarning("------------------------------------");
         return false;
     }
@@ -147,7 +147,7 @@ bool InputAudioFile::load()
         qWarning("------------------------------------");
         qWarning() << "Test audio command:" << (shntool + " " + args.join(" "));
         qWarning() << "shntool info nonzero exit code:" << proc.exitCode();
-        qWarning(proc.readAllStandardError());
+        qWarning() << proc.readAllStandardError();
         qWarning("------------------------------------");
         mErrorString = QObject::tr("File <b>%1</b> is not a supported audio file. <br>"
                                    "<br>Verify that all required programs are installed and in your preferences.").arg(mFileName);
