1813bd0df83e74ce015204589e61ce8f0848eeb7
Alexander Sokolov <sokoloff.a@gmail.com>
2023-02-23 14:27:00 +0300

    Fix build issue under Linux

--- qtbackports/module.cmake
+++ qtbackports/module.cmake
@@ -1,6 +1,11 @@
 list(APPEND SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/movetotrash.h
     ${CMAKE_CURRENT_LIST_DIR}/movetotrash_unix.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/movetotrash_mac.mm
 
 )
+
+if (APPLE)
+    list(APPEND SOURCES
+        ${CMAKE_CURRENT_LIST_DIR}/movetotrash_mac.mm
+    )
+endif()
