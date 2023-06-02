175a323ab790c0b09f252d77186ac0e5d522bea2
SokoloffA <sokoloff.a@gmail.com>
2023-06-02 17:09:55 +0300

    Fix check-extprograms.sh

--- scripts/check-extprograms.sh
+++ scripts/check-extprograms.sh
@@ -8,9 +8,9 @@ BIN_DIR=${2:-${PATH}}
 EXTPROGRAM_H=${SRC_DIR}/extprogram.h
 WHICH=$(which which)
 
-
+PROGS=$(sed -n 's|.*static ExtProgram \*\(.*\)().*|\1|p' "${EXTPROGRAM_H}")
 RES=0
-for PROG in $(sed -n 's|.*static ExtProgram \*\(.*\)().*|\1|p' "${EXTPROGRAM_H}"); do
+for PROG in $PROGS; do
   PATH=$BIN_DIR $WHICH $PROG || RES=1
 done
 exit $RES
