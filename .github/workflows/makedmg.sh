00c626feae8f8f9cc476e813e90f44cd4fa5d74b
Alexander Sokolov <sokoloff.a@gmail.com>
2025-02-15 10:09:50 +0300

    [CI] Notarise application

--- .github/workflows/makedmg.sh
+++ .github/workflows/makedmg.sh
@@ -2,8 +2,8 @@
 # v 0.1
 
 APP_NAME=Flacon
-MAKE_DMG=1
 CERT_IDENTITY="Developer ID Application: Alex Sokolov (635H9TYSZJ)"
+KEYCHAIN_PROFILE="NotaryTool"
 
 #--------------------
 
@@ -28,13 +28,19 @@ DMG_NAME="./${APP_NAME}_${VERSION}.dmg"
 echo "***********************************"
 echo "* App     ${APP_NAME}"
 echo "* Version ${VERSION}"
-echo "* DMG     ${MAKE_DMG}"
+echo "* DMG     ${DMG_NAME}"
 echo "***********************************"
 
-echo "Sign and verify ..............................."
-codesign --force --deep --verify --sign  "${CERT_IDENTITY}" "${BUNDLE_PATH}"
-codesign --all-architectures -v --strict --deep --verbose=1 "${BUNDLE_PATH}"
-spctl --assess --type execute "${BUNDLE_PATH}"
+#rm -rf "${BUNDLE_PATH}/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
+
+echo "Remove quarantine flag ........................"
+chmod  -R u+w "${BUNDLE_PATH}"
+xattr -r -d com.apple.quarantine "${BUNDLE_PATH}"
+echo "  OK"
+
+echo "Sign .........................................."
+codesign --force --options runtime --deep --verify --sign  "${CERT_IDENTITY}" "${BUNDLE_PATH}/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app"
+codesign --force --options runtime --deep --verify --sign  "${CERT_IDENTITY}" "${BUNDLE_PATH}"
 echo "  OK"
 echo ""
 
@@ -44,6 +50,14 @@ spctl --assess --type execute "${BUNDLE_PATH}"
 echo "  OK"
 echo ""
 
-if [[ MAKE_DMG ]]; then
-    dmgbuild -s dmg_settings.json "${BUNDLE_PATH}" "${DMG_NAME}"
-fi
+echo "Create DMG file .............................."
+dmgbuild -s dmg_settings.json "${BUNDLE_PATH}" "${DMG_NAME}"
+echo "  OK"
+
+echo " Notarize ....................................."
+xcrun notarytool submit --wait --keychain-profile "${KEYCHAIN_PROFILE}" "${DMG_NAME}" | tee notarytool-submit.log
+echo "  OK"
+echo ""
+
+echo "Verify notarization ..........................."
+spctl -a -vvv -t install "${BUNDLE_PATH}"
