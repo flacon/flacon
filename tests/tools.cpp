d46295d38703769685168cb4f4c0a9d44fe8f00f
Alexander Sokolov <sokoloff.a@gmail.com>
2025-03-30 16:47:58 +0300

    Issue #243: I need support for the catalog number priunt metadata.json when the test failed.

--- tests/tools.cpp
+++ tests/tools.cpp
@@ -637,6 +637,10 @@ void Mediainfo::validateTags(const QMap<QString, QVariant> &expected)
     }
 
     if (tagsError) {
+        qWarning() << "metadata.json";
+        for (const QByteArray line : mJsonDoc.toJson(QJsonDocument::Indented).split('\n')) {
+            qWarning().noquote().nospace() << line;
+        }
         QFAIL("Some tags not the same");
     }
 }
