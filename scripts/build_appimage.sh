4ac502968464dc1552f65e215e5d2b4cc14e8243
SokoloffA <sokoloff.a@gmail.com>
2026-05-20 20:36:30 +0300

    Build ARM Appimage

---
+++ scripts/build_appimage.sh
@@ -0,0 +1,33 @@
+
+#/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+cd "${SCRIPT_DIR}/.."
+
+function build() {
+    ARCH=$1
+
+    OPTS=""
+    OPTS+=" -b"
+    OPTS+=" --artifact-server-path ./ARTIFACTS"
+    OPTS+=" --workflows .github/workflows/build-appimage.yml"
+    OPTS+=" --matrix name:${ARCH}"
+
+    if [[ "$ARCH" == "arm64" ]]; then
+        OPTS+=" --platform ubuntu-22.04-arm=catthehacker/ubuntu:act-22.04"
+        OPTS+=" --container-architecture linux/arm64"
+        OPTS+=' --container-options "--cpus=8"'
+        #OPTS+=' --container-daemon-opts "--cpus=4"'
+    fi
+
+    act $OPTS
+}
+
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+build x86_64
+#build arm64
